734015b2b2ca96600050ad0a6470d38b188491f7
unknown <johnicassere@hotmail.com>
2024-01-29 01:33:24 -0300

    ajustes mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+        if(posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        }else{
+            exibeElementosDoHeader();
+        }
+
+    })
 
 
     for(let i = 0; i < buttons.length; i++){
@@ -20,6 +33,16 @@ document.addEventListener('DOMContentLoaded', function(){
         
     }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 
 function abreOuFechaResposta (elemento){
@@ -41,8 +64,8 @@ function removeBotaoAtivo(){
 }
 
 function escondeTodasAbas(){
-    const tabsContainer = document.querySelectorAll('[data-tab-id]')
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
     for(let i = 0; i < tabsContainer.length; i++){
-        tabsContainer[i].classList.remove('shows__list--is-active')
+        tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
